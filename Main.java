8f1dd7a3344cf88217e38b9fd2293e391a54b619
Zuhra <zukhrakhonabdurasulova@gmail.com>
2023-08-16 16:55:49 +0500

    updated2

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
    public static void main(String[] args) {
         System.out.println("Hello Git");
        System.out.println("Hello java deveopler");
-       System.out.println("Changed");
+       System.out.println("hit changed");
     }
 }
